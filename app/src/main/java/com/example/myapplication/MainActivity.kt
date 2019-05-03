6a0d409b729a1754f146ee8382d9efafa8f51dc9
ectuser <m.ectuser@gmail.com>
2019-05-03 18:06:04 +0700

    Rls 2: FUCKING PICK IMAGE FROM GALLERY AND CAPTURE IMAGE FROM CAMERA

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -18,15 +18,43 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private val PERMISSION_CODE = 1000;
+    private val IMAGE_PICK_CODE = 1000;
     private val IMAGE_CAPTURE_CODE = 1001
     var image_uri: Uri? = null
+    var REQ_CODE_FOR_ACTION: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         //button click
+        imageButton.setOnClickListener {
+            // FOR GALLERY REQ CODE
+            REQ_CODE_FOR_ACTION = 2002
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED){
+                    //permission denied
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
+                    //show popup to request runtime permission
+                    requestPermissions(permissions, PERMISSION_CODE);
+                }
+                else{
+                    //permission already granted
+                    pickImageFromGallery();
+                }
+            }
+            else{
+                //system OS is < Marshmallow
+                pickImageFromGallery();
+            }
+        }
         cameraButton.setOnClickListener {
+            // FOR CAMERA REQ CODE
+            REQ_CODE_FOR_ACTION = 2001
+
             //if system os is Marshmallow or Above, we need to request runtime permission
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
                 if (checkSelfPermission(Manifest.permission.CAMERA)
@@ -66,10 +94,13 @@ class MainActivity : AppCompatActivity() {
         //called when user presses ALLOW or DENY from Permission Request Popup
         when(requestCode){
             PERMISSION_CODE -> {
-                if (grantResults.size > 0 && grantResults[0] ==
-                    PackageManager.PERMISSION_GRANTED){
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && REQ_CODE_FOR_ACTION == 2001) {
                     //permission from popup was granted
                     openCamera()
+                }
+                else if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && REQ_CODE_FOR_ACTION == 2002) {
+                    //permission from popup was granted
+                    pickImageFromGallery()
                 }
                 else{
                     //permission from popup was denied
@@ -79,12 +110,31 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun pickImageFromGallery() {
+        //Intent to pick image
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+//    companion object {
+//        //image pick code
+//        private val IMAGE_PICK_CODE = 1000;
+//        //Permission code
+//        private val PERMISSION_CODE = 1001;
+//    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         //called when image was captured from camera intent
-        if (resultCode == Activity.RESULT_OK){
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            mainImage.setImageURI(data?.data)
+        }
+        else if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_CAPTURE_CODE){
             //set image captured to image view
             mainImage.setImageURI(image_uri)
         }
     }
+
+
 
 }
