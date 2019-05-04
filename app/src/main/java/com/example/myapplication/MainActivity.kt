ddfe88709c656724b15b6f44f9eb44b2f4b601af
ectuser <m.ectuser@gmail.com>
2019-05-04 12:58:08 +0700

    Fix 1: Comments in the code and OOP SUCKS

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -18,11 +18,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val PERMISSION_CODE = 1000;
-    private val IMAGE_PICK_CODE = 1000;
+    private val PERMISSION_CODE = 1000
+    private val IMAGE_PICK_CODE = 1000
     private val IMAGE_CAPTURE_CODE = 1001
     var image_uri: Uri? = null
     var REQ_CODE_FOR_ACTION: Int = 0
+
+//    private val image = OpenImage()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,66 +33,65 @@ class MainActivity : AppCompatActivity() {
 
         //button click
         imageButton.setOnClickListener {
-            // FOR GALLERY REQ CODE
-            REQ_CODE_FOR_ACTION = 2002
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
-                    PackageManager.PERMISSION_DENIED){
-                    //permission denied
-                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
-                    //show popup to request runtime permission
-                    requestPermissions(permissions, PERMISSION_CODE);
-                }
-                else{
-                    //permission already granted
-                    pickImageFromGallery();
-                }
-            }
-            else{
-                //system OS is < Marshmallow
-                pickImageFromGallery();
-            }
+            checkPermissionsForGallery()
         }
         cameraButton.setOnClickListener {
-            // FOR CAMERA REQ CODE
-            REQ_CODE_FOR_ACTION = 2001
+            checkPermissionsForCamera()
+        }
+    }
 
-            //if system os is Marshmallow or Above, we need to request runtime permission
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                if (checkSelfPermission(Manifest.permission.CAMERA)
-                    == PackageManager.PERMISSION_DENIED ||
-                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                    == PackageManager.PERMISSION_DENIED){
-                    //permission was not enabled
-                    val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                    //show popup to request permission
-                    requestPermissions(permission, PERMISSION_CODE)
-                }
-                else{
-                    //permission already granted
-                    openCamera()
-                }
+
+    // SO NIGGAS THAT'S MY FUCKING CHECK FOR PERMISSIONS OK?
+    private fun checkPermissionsForGallery(){
+        // FOR GALLERY REQ CODE
+        REQ_CODE_FOR_ACTION = 2002
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_DENIED){
+                //permission denied
+                val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                //show popup to request runtime permission
+                requestPermissions(permissions, PERMISSION_CODE)
             }
             else{
-                //system os is < marshmallow
+                //permission already granted
+                pickImageFromGallery()
+            }
+        }
+        else{
+            //system OS is < Marshmallow
+            pickImageFromGallery()
+        }
+    }
+
+    private fun checkPermissionsForCamera(){
+        // FOR CAMERA REQ CODE
+        REQ_CODE_FOR_ACTION = 2001
+
+        //if system os is Marshmallow or Above, we need to request runtime permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (checkSelfPermission(Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED ||
+                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_DENIED){
+                //permission was not enabled
+                val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                //show popup to request permission
+                requestPermissions(permission, PERMISSION_CODE)
+            }
+            else{
+                //permission already granted
                 openCamera()
             }
         }
+        else{
+            //system os is < marshmallow
+            openCamera()
+        }
     }
 
-    private fun openCamera() {
-        val values = ContentValues()
-        values.put(MediaStore.Images.Media.TITLE, "New Picture")
-        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
-        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
-        //camera intent
-        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
-        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
-    }
-
-
+    // SOME KIND OF PERMISSIONS CODE SHIT
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         //called when user presses ALLOW or DENY from Permission Request Popup
         when(requestCode){
@@ -111,6 +112,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // OPEN YOUR CAMERA NIGGA
+    private fun openCamera() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "New Picture")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
+        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        //camera intent
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
+    }
+
+    // OPEN YOUR GALLERY NIGGA
     private fun pickImageFromGallery() {
         //Intent to pick image
         val intent = Intent(Intent.ACTION_PICK)
@@ -118,13 +132,7 @@ class MainActivity : AppCompatActivity() {
         startActivityForResult(intent, IMAGE_PICK_CODE)
     }
 
-//    companion object {
-//        //image pick code
-//        private val IMAGE_PICK_CODE = 1000;
-//        //Permission code
-//        private val PERMISSION_CODE = 1001;
-//    }
-
+    // SAVE YOUR ASS IMAGE NIGGA
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         //called when image was captured from camera intent
         if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
@@ -136,6 +144,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // DMITRY'S FUNCTION TO FUCK SOME BITCHES USING A*
     fun transitToAStar(@Suppress("UNUSED_PARAMETER") view: View) {
         val intent = Intent(this, AStarMainActivity::class.java)
         startActivity(intent)
