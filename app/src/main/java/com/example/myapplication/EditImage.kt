60ab1f7198c117b365bcc0759bd2b6177abb540b
Dmitry <dima.avanesov@yandex.ru>
2019-05-15 21:29:04 +0700

    Rls 4: Interpolation

--- app/src/main/java/com/example/myapplication/EditImage.kt
+++ app/src/main/java/com/example/myapplication/EditImage.kt
@@ -1,7 +1,13 @@
 package com.example.myapplication
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.widget.ImageView
+import android.widget.Toast
+import java.lang.Math.pow
 import kotlin.math.roundToInt
+
 
 class EditImage {
     fun scale(mainImage: ImageView, selected: String, initialHeight: Int) {
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.Color
 import android.graphics.drawable.BitmapDrawable
 import android.media.MediaScannerConnection
 import android.net.Uri
@@ -15,6 +16,7 @@ import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
+import android.view.MotionEvent
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -221,4 +223,8 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    fun transitToInterpolation(@Suppress("UNUSED_PARAMETER") view: View) {
+        val intent = Intent(this, InterpolationActivity::class.java)
+        startActivity(intent)
+    }
 }
